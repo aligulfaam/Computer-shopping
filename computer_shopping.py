30fdad5b2ea7d8e53204760e4393c5caac95cd15
aligulfaamstudent <90401861+aligulfaamstudent@users.noreply.github.com>
2022-03-29 11:22:25 -0400

    first commit

---
+++ computer_shopping.py
@@ -0,0 +1,20 @@
+msi_rtxa5000_price = 4199.35
+gigabyte_aero_price = 4295.54
+razer_blade15_price = 3696.99
+asus_zephyrus_m16_price = 1849.79
+lenovo_gaming_laptop_price = 1499.99
+msi_NIVIDIA_geforce_price = 3099.99
+
+average_price = 3510.42
+
+max = input(f"The Most expensive laptop amount is: {gigabyte_aero_price}")
+min = input(f"The least expensive laptop amount is: {asus_zephyrus_m16_price}")
+round = input(f"The Rounded price of the Msi RTX 5000 is {msi_rtxa5000_price}")
+round = input(f"The Rounded price of the Gigabyte Aero is {gigabyte_aero_price}")
+round = input(f"The Rounded price of the Razer Blade 15 is {razer_blade15_price}")
+round = input(f"The Rounded price of the Aua Zephyrus is {asus_zephyrus_m16_price}")
+round = input(f"The Rounded price of the Lenovo Gaming Laptop is {lenovo_gaming_laptop_price}")
+round = input(f"The Rounded price of the MSI NIVIDIA GeForce is {msi_NIVIDIA_geforce_price}")
+average = input(f"The Average price of all computers is: {average_price}")
+
+print(all, all, all)
